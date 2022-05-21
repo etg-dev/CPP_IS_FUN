589ac7864927f7f2cb239d8b5ff6ddbe1022f16b
etg-dev <taghinia.dev@gmail.com>
2022-05-21 13:05:00 +0430

    TicTacToe is finished

--- L7-TicTacToe/TicTacToe.cpp
+++ L7-TicTacToe/TicTacToe.cpp
@@ -3,18 +3,30 @@
 #include<time.h>
 using namespace std;
 
+int flag=0;
+string sUser;
+string sComputer;
+int computer;
+
 //singniture 
 	void printBoard(string[5][5]);
 	
 	void inputUser(string[5][5],int,string);
 	
-int flag=0;
+	void isFinished(string[5][5],string,string);
+	
+	void isValid(string[5][5]);
+	
+	void tryAgain(string[5][5],int,string);
+	
+
 //main
 	int main(){
 		
 		//decl
 		int user;
-		int computer;
+		
+		int counter;
 		string boardGame[5][5]{
 			{" ","|"," ","|"," "},
 			{"-","+","-","+","-"},
@@ -23,29 +35,33 @@ int flag=0;
 			{" ","|"," ","|"," "}
 		};
 		
-		int counter;
-		while(flag==0&&counter<6){
+		//&&counter<6
+		while(flag==0){
 			//get input
 			cout<<"please Enter Your Position (1-9) : ";
 			cin>>user;
+			sUser=to_string(user);
 			inputUser(boardGame,user,"Human");
 		
 			srand(time(0));
 	    	computer=(rand() % 9 + 1);
+	    	sComputer=to_string(computer);
 	    	inputUser(boardGame,computer,"Computer");
 	    
 	    	
-	
 			//call
 			printBoard(boardGame);
-			
 			counter++;
-		
+			
+			if(counter==5){
+				cout<<"DRAW";
+				flag=1;
+			}
+			
 		}
 		
-		
 		return 0;
-	}//main
+	}
 
 
 //function 
@@ -60,8 +76,91 @@ int flag=0;
     	
 	}
 
+	void isFinished(string boardGame[5][5],string rec,string id){
+		
+		if(boardGame[0][0]==rec&&boardGame[0][2]==rec&&boardGame[0][4]==rec&&id=="Human"){
+	    	cout<<"***YOU WIN***"<<endl;
+	    	flag=1;
+		}
+		else if(boardGame[2][0]==rec&&boardGame[2][2]==rec&&boardGame[2][4]==rec&&id=="Human"){
+	    	cout<<"***YOU WIN***"<<endl;
+	    	flag=1;
+		}
+		else if(boardGame[4][0]==rec&&boardGame[4][2]==rec&&boardGame[4][4]==rec&&id=="Human"){
+	    	cout<<"***YOU WIN***"<<endl;
+	    	flag=1;
+		}
+		else if(boardGame[0][0]==rec&&boardGame[2][0]==rec&&boardGame[4][0]==rec&&id=="Human"){
+	    	cout<<"***YOU WIN***"<<endl;
+	    	flag=1;
+		}
+		else if(boardGame[0][2]==rec&&boardGame[2][2]==rec&&boardGame[4][2]==rec&&id=="Human"){
+	    	cout<<"***YOU WIN***"<<endl;
+	    	flag=1;
+		}
+		else if(boardGame[0][4]==rec&&boardGame[2][4]==rec&&boardGame[4][4]==rec&&id=="Human"){
+	    	cout<<"***YOU WIN***"<<endl;
+	    	flag=1;
+		}
+		else if(boardGame[0][0]==rec&&boardGame[2][2]==rec&&boardGame[4][4]==rec&&id=="Human"){
+	    	cout<<"***YOU WIN***"<<endl;
+	    	flag=1;
+		}
+		else if(boardGame[0][4]==rec&&boardGame[2][2]==rec&&boardGame[4][0]==rec&&id=="Human"){
+	    	cout<<"***YOU WIN***"<<endl;
+	    	flag=1;
+		}
+		
+		//computer
+		else if(boardGame[0][0]==rec&&boardGame[0][2]==rec&&boardGame[0][4]==rec&&id=="Computer"){
+	    	cout<<"***YOU LOST***"<<endl;
+	    	flag=1;
+		}
+		else if(boardGame[2][0]==rec&&boardGame[2][2]==rec&&boardGame[2][4]==rec&&id=="Computer"){
+	    	cout<<"***YOU LOST***"<<endl;
+	    	flag=1;
+		}
+		else if(boardGame[4][0]==rec&&boardGame[4][2]==rec&&boardGame[4][4]==rec&&id=="Computer"){
+	    	cout<<"***YOU LOST***"<<endl;
+	    	flag=1;
+		}
+		else if(boardGame[0][0]==rec&&boardGame[2][0]==rec&&boardGame[4][0]==rec&&id=="Computer"){
+	    	cout<<"***YOU LOST***"<<endl;
+	    	flag=1;
+		}
+		else if(boardGame[0][2]==rec&&boardGame[2][2]==rec&&boardGame[4][2]==rec&&id=="Computer"){
+	    	cout<<"***YOU LOST***"<<endl;
+	    	flag=1;
+		}
+		else if(boardGame[0][4]==rec&&boardGame[2][4]==rec&&boardGame[4][4]==rec&&id=="Computer"){
+	    	cout<<"***YOU LOST***"<<endl;
+	    	flag=1;
+		}
+		else if(boardGame[0][0]==rec&&boardGame[2][2]==rec&&boardGame[4][4]==rec&&id=="Computer"){
+	    	cout<<"***YOU LOST***"<<endl;
+	    	flag=1;
+		}
+		else if(boardGame[0][4]==rec&&boardGame[2][2]==rec&&boardGame[4][0]==rec&&id=="Computer"){
+	    	cout<<"***YOU LOST***"<<endl;
+	    	flag=1;
+		}
+	}
+
+	void isValid(string boardGame[5][5],string rec,string id){
+		int temp;
+		for (int i = 0; i < 5; i++){
+	        for (int j = 0; j < 5; j++){
+	        	if(boardGame[i][j]==rec){
+	        		cout<<i<<" "<<j<<endl;
+				}
+	        }
+    	}
+	}
+	
 	void inputUser(string boardGame[5][5],int user,string id){
+		
 		string rec;
+		
 		if(id=="Human"){
 			rec="X";
 		}else if(id=="Computer"){
@@ -72,133 +171,91 @@ int flag=0;
 		
 		switch(user){
 			case 1:
-			
-					boardGame[0][0]=rec;
-			
+				if(boardGame[0][0]=="X"||boardGame[0][0]=="O"){
+					tryAgain(boardGame,user,id);
+				}else{
+					boardGame[0][0]=rec;		
+				}
 			break;
 			case 2:
-			
+				if(boardGame[0][2]=="X"||boardGame[0][2]=="O"){
+					tryAgain(boardGame,user,id);
+				}else{
 					boardGame[0][2]=rec;
-			
+				}
 			break;
 			case 3:
-			
+				if(boardGame[0][4]=="X"||boardGame[0][4]=="O"){
+					tryAgain(boardGame,user,id);
+				}else{
 					boardGame[0][4]=rec;
-				
+				}
 			break;
 			case 4:
-			
+				if(boardGame[2][0]=="X"||boardGame[2][0]=="O"){
+					tryAgain(boardGame,user,id);
+				}else{
 					boardGame[2][0]=rec;
-			
+				}
 			break;
 			case 5:
-			
+				if(boardGame[2][2]=="X"||boardGame[2][2]=="O"){
+					tryAgain(boardGame,user,id);
+				}else{
 					boardGame[2][2]=rec;
-			
+				}
 			break;
 			case 6:
-			
+				if(boardGame[2][4]=="X"||boardGame[2][4]=="O"){
+					tryAgain(boardGame,user,id);
+				}else{
 					boardGame[2][4]=rec;
-			
+				}
 			break;
 			case 7:
-			
+				if(boardGame[4][0]=="X"||boardGame[4][0]=="O"){
+					tryAgain(boardGame,user,id);
+				}else{
 					boardGame[4][0]=rec;
-			
+				}
 			break;
 			case 8:
-			
+				if(boardGame[4][2]=="X"||boardGame[4][2]=="O"){
+					tryAgain(boardGame,user,id);
+				}else{
 					boardGame[4][2]=rec;
-			
+				}
 			break;
 			case 9:
-			
+				if(boardGame[4][4]=="X"||boardGame[4][4]=="O"){
+					tryAgain(boardGame,user,id);
+				}else{
 					boardGame[4][4]=rec;
-			
+				}
 			break;	
 			default :
 				cout<<"Your input is wrong!";
 			break;
 		}
 		
-		//int count;
-		if(boardGame[0][0]==rec){
-			if (rec.find("X") != string::npos) {
-				cout<<"X FOUND"<<endl;
-			//	count++;
-			} 
-		}
+		isFinished(boardGame,rec,id);
 		
-		if(boardGame[0][0]==rec&&boardGame[0][2]==rec&&boardGame[0][4]==rec&&id=="Human"){
-	    	cout<<"***YOU WIN***"<<endl;
-	    	flag=1;
-		}
-		if(boardGame[2][0]==rec&&boardGame[2][2]==rec&&boardGame[2][4]==rec&&id=="Human"){
-	    	cout<<"***YOU WIN***"<<endl;
-	    	flag=1;
-		}
-		if(boardGame[4][0]==rec&&boardGame[4][2]==rec&&boardGame[4][4]==rec&&id=="Human"){
-	    	cout<<"***YOU WIN***"<<endl;
-	    	flag=1;
-		}
-		if(boardGame[0][0]==rec&&boardGame[2][0]==rec&&boardGame[4][0]==rec&&id=="Human"){
-	    	cout<<"***YOU WIN***"<<endl;
-	    	flag=1;
-		}
-		if(boardGame[0][2]==rec&&boardGame[2][2]==rec&&boardGame[4][2]==rec&&id=="Human"){
-	    	cout<<"***YOU WIN***"<<endl;
-	    	flag=1;
-		}
-		if(boardGame[0][4]==rec&&boardGame[2][4]==rec&&boardGame[4][4]==rec&&id=="Human"){
-	    	cout<<"***YOU WIN***"<<endl;
-	    	flag=1;
-		}
-		if(boardGame[0][0]==rec&&boardGame[2][2]==rec&&boardGame[4][4]==rec&&id=="Human"){
-	    	cout<<"***YOU WIN***"<<endl;
-	    	flag=1;
-		}
-		if(boardGame[0][4]==rec&&boardGame[2][2]==rec&&boardGame[4][0]==rec&&id=="Human"){
-	    	cout<<"***YOU WIN***"<<endl;
-	    	flag=1;
-		}
-		
-		//computer
-		if(boardGame[0][0]==rec&&boardGame[0][2]==rec&&boardGame[0][4]==rec&&id=="Computer"){
-	    	cout<<"***YOU LOST***"<<endl;
-	    	flag=1;
-		}
-		if(boardGame[2][0]==rec&&boardGame[2][2]==rec&&boardGame[2][4]==rec&&id=="Computer"){
-	    	cout<<"***YOU LOST***"<<endl;
-	    	flag=1;
-		}
-		if(boardGame[4][0]==rec&&boardGame[4][2]==rec&&boardGame[4][4]==rec&&id=="Computer"){
-	    	cout<<"***YOU LOST***"<<endl;
-	    	flag=1;
-		}
-		if(boardGame[0][0]==rec&&boardGame[2][0]==rec&&boardGame[4][0]==rec&&id=="Computer"){
-	    	cout<<"***YOU LOST***"<<endl;
-	    	flag=1;
-		}
-		if(boardGame[0][2]==rec&&boardGame[2][2]==rec&&boardGame[4][2]==rec&&id=="Computer"){
-	    	cout<<"***YOU LOST***"<<endl;
-	    	flag=1;
-		}
-		if(boardGame[0][4]==rec&&boardGame[2][4]==rec&&boardGame[4][4]==rec&&id=="Computer"){
-	    	cout<<"***YOU LOST***"<<endl;
-	    	flag=1;
-		}
-		if(boardGame[0][0]==rec&&boardGame[2][2]==rec&&boardGame[4][4]==rec&&id=="Computer"){
-	    	cout<<"***YOU LOST***"<<endl;
-	    	flag=1;
-		}
-		if(boardGame[0][4]==rec&&boardGame[2][2]==rec&&boardGame[4][0]==rec&&id=="Computer"){
-	    	cout<<"***YOU LOST***"<<endl;
-	    	flag=1;
-		}
-		
-	//	cout<<count<<endl;
+	//	isValid(boardGame,rec,id);
 		
 	}
-
-
-
+	
+	void tryAgain(string boardGame[5][5],int user,string id){
+		if(id=="Human"){
+			cout<<"Your input is not valid TRY AGAIN"<<endl;
+			cout<<"please Enter Your Position (1-9) : ";
+			cin>>user;
+			inputUser(boardGame,user,"Human");
+		}
+		else if(id=="Computer"){
+	    	computer=(rand() % 9 + 1);
+	    	inputUser(boardGame,computer,"Computer");
+		}	
+	}
+	
+	
+	
